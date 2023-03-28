79f2af54cbcddd4962523432e4d0ae2be7258f66
yathu-micro <129233770+yathu-micro@users.noreply.github.com>
2023-03-29 07:21:12 +0800

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,77 @@
+input.onButtonPressed(Button.A, function () {
+    if (bits > 0) {
+        bits = bits - 1
+    } else {
+        if (channel_no > decimal) {
+            basic.showNumber(decimal)
+            basic.showLeds(`
+                . . # . .
+                . . # . .
+                # # # # #
+                . . # . .
+                . . # . .
+                `)
+        } else if (channel_no < decimal) {
+            basic.showLeds(`
+                . . . . .
+                . . . . .
+                # # # # #
+                . . . . .
+                . . . . .
+                `)
+        } else {
+            basic.showLeds(`
+                # # . # #
+                # # . # #
+                . . . . .
+                # . . . #
+                . # # # .
+                `)
+        }
+        decimal = 0
+        bits = 7
+    }
+})
+input.onButtonPressed(Button.B, function () {
+    decimal = 2 ** bits + decimal
+    basic.showNumber(channel_no)
+    if (bits > 0) {
+        bits = bits - 1
+    } else {
+        if (channel_no > decimal) {
+            basic.showNumber(decimal)
+            basic.showLeds(`
+                . . # . .
+                . . # . .
+                # # # # #
+                . . # . .
+                . . # . .
+                `)
+        } else if (channel_no < decimal) {
+            basic.showLeds(`
+                . . . . .
+                . . . . .
+                # # # # #
+                . . . . .
+                . . . . .
+                `)
+        } else {
+            basic.showLeds(`
+                # # . # #
+                # # . # #
+                . . . . .
+                # . . . #
+                . # # # .
+                `)
+        }
+        decimal = 0
+        bits = 7
+    }
+})
+let bits = 0
+let channel_no = 0
+let decimal = 0
+let binary = ""
+decimal = 0
+channel_no = 1
+bits = 7
